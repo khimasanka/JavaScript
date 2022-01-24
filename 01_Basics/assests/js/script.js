151d38d84030260899fdf33ece266ad2d3d1bec1
sanuv9683 <sanuv9683@gmail.com>
2022-01-24 09:58:49 +0530

    Content Updated

---
+++ 01_Basics/assests/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+/*we can write js in a externel file also*/
+/*
+document.getElementById("");*/
+
+
+console.log("Im from External File");
